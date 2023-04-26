546abac20b7c9c9ae7e7ea4ee996c408bb499580
felipyroma <felipy.roma@sptech.school>
2023-04-26 14:10:27 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/stefanini/course/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.stefanini.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.stefanini.course.entities.Order;
+import com.stefanini.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
